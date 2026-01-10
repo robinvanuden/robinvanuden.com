d93b3b5c8d4c1e7329b9cb31e79b8d09b9ab007e
robin <robinvanuden@gmail.com>
2026-01-10 10:28:14 +0100

    Removed eslint-plugin-prettier, updated scripts and dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
-import recommendedConfig from "eslint-plugin-prettier/recommended";
 
 // Your custom configs here
-export default withNuxt([recommendedConfig]);
+export default withNuxt();
