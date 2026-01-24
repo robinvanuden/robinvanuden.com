e3006f98887004606e37bc008b27f6dfaa41f00a
robin <robinvanuden@gmail.com>
2026-01-24 08:57:18 +0100

    Installed Nuxt UI and Eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,46 @@
-import tailwindcss from "@tailwindcss/vite";
-
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    "@nuxt/ui",
     "@nuxt/content",
     "@nuxt/eslint",
-    "@nuxt/fonts",
     "@nuxt/image",
     "@nuxtjs/sitemap",
     "@nuxtjs/robots",
   ],
+  css: ["~/assets/tailwind.css"],
   site: {
     indexable: true,
     url: process.env.NUXT_PUBLIC_SITE_URL,
     name: process.env.NUXT_PUBLIC_SITE_NAME,
   },
-  runtimeConfig: {
-    public: {
-      url: process.env.NUXT_PUBLIC_SITE_URL,
-      name: process.env.NUXT_PUBLIC_SITE_NAME,
-      email: process.env.NUXT_PUBLIC_SITE_EMAIL,
-      socials: {
-        github: process.env.NUXT_PUBLIC_SOCIAL_GITHUB_URL,
-        instagram: process.env.NUXT_PUBLIC_SOCIAL_INSTAGRAM_URL,
-        linkedin: process.env.NUXT_PUBLIC_SOCIAL_LINKEDIN_URL,
-        letterboxd: process.env.NUXT_PUBLIC_SOCIAL_LETTERBOXD_URL,
-        pinterest: process.env.NUXT_PUBLIC_SOCIAL_PINTEREST_URL,
-      },
+  ui: {
+    theme: {
+      colors: [
+        "primary",
+        "secondary",
+        "tertiary",
+        "info",
+        "success",
+        "warning",
+        "error",
+      ],
     },
   },
-  css: [
-    "@fortawesome/fontawesome-svg-core/styles.css",
-    "~/assets/tailwind.css",
-  ],
+  runtimeConfig: {
+    public: {
+      site: {
+        url: "",
+        name: "",
+        email: "",
+      },
+      socials: {
+        github: "",
+        instagram: "",
+        linkedin: "",
+        letterboxd: "",
+        pinterest: "",
+      },
+  },
   fonts: {
     families: [
       {
@@ -42,20 +50,8 @@ export default defineNuxtConfig({
       },
     ],
   },
-  app: {
-    head: {
-      link: [
-        {
-          rel: "icon",
-          type: "image/x-icon",
-          href: "/favicon.ico",
-        },
-      ],
-    },
-  },
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   ssr: true,
-  vite: { plugins: [tailwindcss()] },
   typescript: { strict: true, typeCheck: true },
 });
